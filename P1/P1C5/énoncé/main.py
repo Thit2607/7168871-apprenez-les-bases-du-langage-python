663891489f72c0af4f8f0018d701414d0fd5d579
Thit2607 <thiiicroon@gmail.com>
2025-02-18 20:05:49 +0100

    Update main.py

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,17 @@
 # Écrivez votre code ici !
+nom = "Thierry"
+age = 57
+taille = 1.70
+est_etudiant = True
+print (f"Nom: {nom}")
+print (f"Age: {age}")
+print (f"Taille: {taille}")
+print (f"Est étudiant: {est_etudiant}")
+type_nom = type (nom)
+type_age = type (age)
+type_taille = type (taille)
+type_etudiant = type (est_etudiant)
+print (f"Type nom: {type_nom}")
+print (f"Type age: {type_age}")
+print (f"Type taille: {type_taille}")
+print (f"Type est étudiant: {type_etudiant}")
