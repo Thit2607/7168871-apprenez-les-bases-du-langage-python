877357b36ce502a5cd37d1db2d056fb7a3e43242
Thit2607 <thiiicroon@gmail.com>
2025-02-19 20:35:55 +0100

    Update main.py

--- P1/P1C7/énoncé/main.py
+++ P1/P1C7/énoncé/main.py
@@ -1,1 +1,9 @@
-# Écrivez votre code ici !
+fruits={'pomme':'rouge','banane':'jaune','orange':'orange'}
+fruits['kiwi']='vert'
+print(fruits)
+couleur_banane=fruits.get('banane')
+print(f"la banane est {couleur_banane} !!")
+fruits['pomme']='vert'
+print(fruits)
+fruits.pop('banane')
+print(fruits)
