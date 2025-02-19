9991d4b7b0120eb178cb4c8301b6d7beef5e202d
Thit2607 <thiiicroon@gmail.com>
2025-02-19 18:48:15 +0100

    Update main.py

--- P1/P1C4/énoncé/main.py
+++ P1/P1C4/énoncé/main.py
@@ -1,1 +1,5 @@
-# Écrivez votre code ici !
+nom = "Thierry"
+age = 57
+print("Bonjour, je m'appelle", nom, "et j'ai", age, "ans.")
+age = age + 10
+print("Je m'appelle", nom, "et j'ai", age, "ans maintenant.")
